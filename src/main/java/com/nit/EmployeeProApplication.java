b76932ac699a665592b34f5a0c1b55bbc93634e3
91852 <91852@DESKTOP-7436BH1>
2022-05-19 00:37:49 +0530

    1st code commit

--- src/main/java/com/nit/EmployeeProApplication.java
+++ src/main/java/com/nit/EmployeeProApplication.java
@@ -8,6 +8,8 @@ public class EmployeeProApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeProApplication.class, args);
+		
+		System.out.println("hello");
 	}
 
 }
